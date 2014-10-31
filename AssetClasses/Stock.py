ff1aa2fb4d720219f9b48047f4eb625796027975
Fei Chen <fchen67@gmail.com>
2014-11-01 05:25:52 +0800

    test

--- AssetClasses/Stock.py
+++ AssetClasses/Stock.py
@@ -1,24 +1,39 @@
 # coding=gbk
 #import numpy module
 
-from datetime import datetime
+import os
 import codecs
+import datetime
 
 import matplotlib.pyplot as py_plot
 import matplotlib.finance as finance_plot
 
 
 class Stock():
+# This is the class for Stock
 
+    # daily quotes of a stock, each quote contains the sequence
+    # [date, open price, highest price, lowest price, close price, daily volume, total trading amount]
     quotes = []
 
+    def __init__(self):
+    # Initializer
+        print("A stock object is created.")
+
     def import_stock_data_from_zszq(self, path, identifier):
+    # This method loads trading data of a specified stock from Zhaoshang Securities data
 
         # path of the data source
-        path = path + "/" + identifier + ".txt"
+        if path[-1] == "/":
+            path = path + identifier + ".txt"
+        else:
+            path = path + "/" + identifier + ".txt"
 
         # read data
-        file_input = codecs.open(path, "r", "gbk").readlines()
+        if os.path.isfile(path):
+            file_input = codecs.open(path, "r", "gbk").readlines()
+        else:
+            raise IOError("File Not Found.")
 
         # header - id, stock name
         ##header1 = input[0].split(" ")
@@ -50,7 +65,7 @@ class Stock():
         # generate price vectors
         for day in prices:
             data = day.split("\t")
-            pdate = finance_plot.date2num(datetime.strptime(data[header["date"]].strip(), "%Y/%m/%d"))
+            pdate = finance_plot.date2num(datetime.datetime.strptime(data[header["date"]].strip(), "%Y/%m/%d"))
             popen = float(data[header["open"]].strip())
             phighest = float(data[header["highest"]].strip())
             plowest = float(data[header["lowest"]].strip())
@@ -60,6 +75,7 @@ class Stock():
 
             self.quotes.append([pdate, popen, phighest, plowest, pclose, pvolume, ptotal])
 
+        print("Quotes of stock {0} from date {1} to {2} have been loaded".format(identifier, datetime.date.fromordinal(int(self.quotes[0][0])), datetime.date.fromordinal(int(self.quotes[-1][0]))))
         return 0
 
 
@@ -124,8 +140,7 @@ class Stock():
     #
     #     return [priceDate, priceOpen, priceHighest, priceLowest, priceClose, volume, total]
 
-
-    def japanese_candle_chart(self):
+    def japanese_candle_chart(self, date_start, date_end):
         # This method takes stock price data and generates the japanese candle chart
 
         #quotes = []
@@ -149,4 +164,5 @@ class Stock():
 if __name__ == "__main__":
     stock = Stock()
     stock.import_stock_data_from_zszq("/home/fei/Documents/Me/Computing/Data", "SH600674")
+    #stock.import_stock_data_from_zszq("/wrong", "SH600674")
     stock.japanese_candle_chart()
